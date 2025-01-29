12b59be19b3a16250d02746260a349964125afdc
Yit-hub <yidanielchen.23@campuscamara.es>
2025-01-30 00:23:40 +0100

    script added

---
+++ script.sql
@@ -0,0 +1,18 @@
+CREATE TABLE usuarios (
+	id INT,
+	nombre VARCHAR(30) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	PRIMARY KEY(id)
+)
+INSERT INTO usuarios (id, nombre, email) VALUES
+(1, 'Carlos Pérez', 'carlos.perez@email.com'),
+(2, 'María González', 'maria.gonzalez@email.com'),
+(3, 'Juan Ramírez', 'juan.ramirez@email.com'),
+(4, 'Laura Torres', 'laura.torres@email.com'),
+(5, 'Pedro Sánchez', 'pedro.sanchez@email.com'),
+(6, 'Ana Martínez', 'ana.martinez@email.com'),
+(7, 'Javier López', 'javier.lopez@email.com'),
+(8, 'Sofía Herrera', 'sofia.herrera@email.com'),
+(9, 'Diego Fernández', 'diego.fernandez@email.com'),
+(10, 'Elena Castro', 'elena.castro@email.com'),
+(11,"Daniel","yithub@gmail.com");
